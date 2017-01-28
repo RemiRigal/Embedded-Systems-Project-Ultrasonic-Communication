59ede7c2473c6f182f81c449369a795c71eca136
Noelie Ramuzat <ramuzat.noelie@gmail.com>
2017-01-29 00:29:12 +0100

    [Main] Bin update [USReceiver] Gpio & frequency fix + add of blueLed blinking

--- Code/Utilities/USReceiver.cpp
+++ Code/Utilities/USReceiver.cpp
@@ -5,10 +5,12 @@
 
 using namespace miosix;
 
-typedef Gpio<GPIOH_BASE, 1> receiver;
+typedef Gpio<GPIOA_BASE, 0> receiver;
+typedef Gpio<GPIOD_BASE, 15> blueLed;
 
 USReceiver::USReceiver() {
 	receiver::mode(Mode::INPUT);
+	blueLed::mode(Mode::OUTPUT);
 }
 
 USReceiver::~USReceiver() {
@@ -18,27 +20,34 @@ USReceiver::~USReceiver() {
 std::vector<int> USReceiver::receive() {
 	int lastPoll = 0;
 	int squareCount = 0;
+	int zeroCount = 0;
 	int lastCharacter = -1;
 	textEnded = false;
 	textStarted = false;
 	message.clear();
 	
 	while (!textEnded) {
-		delayUs(POLLING_RATE);
+		delayUs(12);
 		
 		int poll = receiver::value();
 		if (poll == 1) {
+			blueLed::high();
+			if (zeroCount != 0) {
+				zeroCount = 0;
+			}
 			if (lastPoll == 0) { // 1;0
 				// New square coming
 				squareCount += 1;
 			}
 		} else {
+			blueLed::low();
 			if (lastPoll == 1) { // 0;1
 				// End of square
-			} else { // 0;0
+			} else if (squareCount > 50 && zeroCount > 50) { // 0;0
 				// End of character
-				int character = (squareCount / 100) - 1;
-				
+				zeroCount = 0;
+				int character = ((squareCount  + 50) / 100) - 1;
+				printf("%i\n", character);
 				if (textStarted) {
 					if (lastCharacter != -1) {
 						textEnded = Decoder::isEndOfText(lastCharacter, character);
@@ -59,6 +68,8 @@ std::vector<int> USReceiver::receive() {
 					lastCharacter = character;
 				}
 				squareCount = 0;
+			} else if (squareCount > 50) {
+				zeroCount++;
 			}
 		}
 		lastPoll = poll;
