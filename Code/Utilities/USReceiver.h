d56b6f7e6f62c7cea077bf38a7a3f5ee13a886c1
Rémi Rigal <rigal.remi@gmail.com>
2017-02-01 16:15:37 +0100

    [USReceiver] Add constants in header

--- Code/Utilities/USReceiver.h
+++ Code/Utilities/USReceiver.h
@@ -17,6 +17,8 @@ class USReceiver {
 	
 		const int FREQUENCY = 40000;
 		const int POLLING_RATE = 1000000 / FREQUENCY;
+		const int SQUARE_MULTIPLIER = 2000;
+		const int HALF_SQUARE_MULTIPLIER = SQUARE_MULTIPLIER / 2;
 		
 		bool textEnded = false;
 		bool textStarted = false;
